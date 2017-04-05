bddcf62f8fed73a5077f47a8e43b81e550f4324b
DaruoMatrix <mourad.riahi@esprit.tn>
2017-04-05 16:21:05 +0100

    add sharing commute

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 //var app = require('express')();
 var mongoose = require('mongoose');
 var fs = require('fs');
-var cors = require('cors');
+//var cors = require('cors');
 var bodyParser = require('body-parser');
-
 var express = require("express");
-var app     = express();
+var app = express();
+// view engine setup
 
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'twig');
 //Connect to the Database and load all models
 mongoose.connect('mongodb://localhost/photon');
 fs.readdirSync(__dirname + '/models').forEach(function(model){
@@ -14,19 +16,19 @@ fs.readdirSync(__dirname + '/models').forEach(function(model){
     require(__dirname + '/models/' + model);
     console.info(model +' Loaded!');
   }
-})
+});
 
 //require routes
 var users = require('./routes/users');
 var seeder = require('./routes/seeder');
 var cars = require('./routes/cars');
 var schedules = require('./routes/schedules');
-
+var sharing = require('./routes/sharing');
 app.use(express.static(__dirname + '/public'));
 var commutes = require('./routes/commutes');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+//pp.use(cors());
 
 
 //routes
@@ -35,8 +37,8 @@ app.use('/seeder', seeder);
 app.use('/cars', cars);
 app.use('/schedules', schedules);
 app.use('/commutes', commutes);
-
+app.use('/sharing', sharing);
 app.listen(4000, function(){
   console.log('server started on port 4000');
 
-})
+});
